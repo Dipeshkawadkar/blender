de9e1a1670c6b368a9dfa58c6ab90c33cbad71de
Dipeshkawadkar <33801752+Dipeshkawadkar@users.noreply.github.com>
2021-05-24 01:09:04 +0530

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,117 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import gsap from 'gsap'
+
+const gltfLoader = new GLTFLoader()
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Car
+let tl = gsap.timeline()
+
+gltfLoader.load('finalCar.gltf', (gltf) => {
+
+    gltf.scene.scale.set(0.4, 0.4, 0.4)
+    gltf.scene.rotation.set(0.47, 3.7, 0.0)
+
+    scene.add(gltf.scene)
+
+    gui.add(gltf.scene.rotation, 'x').min(0).max(9)
+    gui.add(gltf.scene.rotation, 'y').min(0).max(9)
+    gui.add(gltf.scene.rotation, 'z').min(0).max(9)
+
+    tl.to(gltf.scene.rotation, { x: 0.6, y: 0.22, z: 0.21, duration: 1 })
+    tl.to(gltf.scene.scale, { x: 0.1, y: 0.1, z: 0.1, duration: 1 }, "-=1")
+    tl.to(gltf.scene.position, { z: 0.2 })
+
+    tl.to(gltf.scene.rotation, { x: 0.21, y: 0.42, z: 0.1, duration: 1.2 })
+    tl.to(gltf.scene.scale, { x: 0.25, y: 0.25, z: 0.25, duration: 1 }, "-=1")
+
+})
+
+// Lights
+const pointLight = new THREE.DirectionalLight( 0xffffff, 1, 100 );
+
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update Orbital Controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
